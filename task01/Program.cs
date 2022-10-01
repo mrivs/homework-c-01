ab7bfcf0474db6adaf7b47d1e41566e2f2e3ed60
mrivs <valentinilyins@gmail.com>
2022-10-01 17:29:03 +0300

    task04 added

--- task01/Program.cs
+++ task01/Program.cs
@@ -5,11 +5,8 @@ a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3 */
 
 Console.WriteLine("Введите число 1");
-
 int a=int.Parse(Console.ReadLine());
-
 Console.WriteLine("Введите число 2");
-
 int b=int.Parse(Console.ReadLine());
 
 if (a>b) Console.WriteLine("max = " + a );
---
+++ task04/Program.cs
@@ -0,0 +1,18 @@
+﻿/*/
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+/*/
+
+Console.WriteLine("Введите число");
+int N=int.Parse(Console.ReadLine());
+string str="";
+
+for (int i = 1;i<=N;i++) {
+
+    if (i%2==0) str=str+i+", ";
+}
+
+str=str.Substring(0, str.Length - 2);
+Console.WriteLine(str);
